32c3582eef0ae65267ae78741f41ee094b3ea135
Sam Baldwin <sambaldwin92@gmail.com>
2023-07-13 13:51:18 +0100

    Controllers created

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -13,4 +13,19 @@ export const getTodos = async (req, res) => {
         console.error(err.message);
     }
 }
-    
+
+// Function to add a new todo to the database
+export const addTodo = async (req, res) => {
+    // Try and catch block to handle errors
+    try {
+        // Call the addTodo function from todoModel.js
+        const newTodo = await todosModel.addTodo(
+            req.body.description
+        );
+        // Send the new todo as a response
+        res.json({ success: true, payload: newTodo });
+    } catch (err) {
+        // Catch block to handle errors
+        console.error(err.message);
+    }
+}
