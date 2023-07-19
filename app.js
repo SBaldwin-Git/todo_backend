dc8966934e3c5bc5cf7059291eca2a338c19755f
Sam Baldwin <sambaldwin92@gmail.com>
2023-07-19 16:19:38 +0100

    Tested routes successfully!

--- app.js
+++ app.js
@@ -7,10 +7,14 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(morgan('dev'));
+// LOADS FROM THE PUBLIC FOLDER WHICH IS THE HTML FILE
 app.use(express.static('public'));
 app.use(express.json());
 
+// THE URL FOR THE ROUTES IS /api/todos
 app.use('/api/todos', router);
+
+app.use(express.static("public"));
 
 app.listen(PORT, function () {
   console.log(`Server listening on port http://localhost:${PORT}`);
