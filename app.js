a75ddb0aa9f44d3a6b129930f800a968995ce79a
Sam Baldwin <sambaldwin92@gmail.com>
2023-07-19 16:52:06 +0100

    Added cors for local testing

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 
 import { router } from './routes/todoRoute.js';
 
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(cors());
 app.use(morgan('dev'));
 // LOADS FROM THE PUBLIC FOLDER WHICH IS THE HTML FILE
 app.use(express.static('public'));
